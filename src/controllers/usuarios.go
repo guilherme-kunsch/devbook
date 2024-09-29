e186b917caa2d185115892ee0dcdecce5b96ef20
guilherme-kunsch <guilherme.kunsch@outlook.com>
2024-09-29 19:06:33 -0300

    criando pacote response

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -4,37 +4,42 @@ import (
 	"api/src/banco"
 	"api/src/models"
 	"api/src/repositories"
+	"api/src/response"
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 )
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		log.Fatal(err)
+		response.Erro(w, http.StatusUnprocessableEntity, err)
+		return
 	}
 
 	var user models.User
 	if err = json.Unmarshal(body, &user); err != nil {
-		log.Fatal(err)
+		response.Erro(w, http.StatusBadRequest, err)
+		return
 	}
 	fmt.Println(user)
 
 	db, err := banco.Conection()
 	if err != nil {
-		log.Fatal(err)
+		response.Erro(w, http.StatusInternalServerError, err)
+		return
 	}
 
 	repository := repositories.NewRepositoryUsers(db)
-	userID, err := repository.Create(user)
+	user.ID, err = repository.Create(user)
 	if err != nil {
-		log.Fatal(err)
+		response.Erro(w, http.StatusInternalServerError, err)
+		return
 	}
 
-	w.Write([]byte(fmt.Sprintf("ID entered: %d", userID)))
+	response.JSON(w, http.StatusCreated, user)
+
 }
 
 func SearchUsers(w http.ResponseWriter, r *http.Request) {
---
+++ src/response/response.go
@@ -0,0 +1,23 @@
+package response
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.WriteHeader(statusCode)
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Erro(w http.ResponseWriter, statusCode int, erro error) {
+	JSON(w, statusCode, struct {
+		Erro string `json:"erro"`
+	}{
+		Erro: erro.Error(),
+	})
+}
